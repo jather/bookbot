39f74e72640077de9beb683442d6952a202e7e38
jather <31395687+jather@users.noreply.github.com>
2024-12-29 17:27:27 +0900

    count words, count letters, return sorted letter count

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+def main(book):
+    print(f"--- Begin report of {book}")
+    with open(book) as f:
         file_contents = f.read()
-    print(file_contents)
-    print(wordcount(file_contents))
+        print(f"{wordcount(file_contents)} words found in the document")
+        letter_count = countletter(file_contents)
+        letter_list = []
+        for letter in letter_count:
+            if letter.isalpha():
+                letter_list.append([letter, letter_count[letter]])
+        letter_list.sort()
+        print(letter_list)
+
 
 
 def wordcount(words):
@@ -11,4 +19,16 @@ def wordcount(words):
         count += 1
     return count
 
-main()
+def countletter(words):
+    lettercounts = {}
+    lowercase_words = words.lower()
+    for letter in lowercase_words:
+        if letter in lettercounts:
+            lettercounts[letter] += 1
+        else:
+            lettercounts[letter] = 1
+    return lettercounts
+
+main("books/frankenstein.txt")
+
+
